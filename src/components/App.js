18a9c8cd96d2a2b410a9dd08b0fe0fa0368df6a2
Your Name <you@example.com>
2021-03-30 05:18:02 +0900

    이메일 로그인 완성

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import {firebaseInstance} from 'fbase';
 import {authService} from 'fbase';
 
 function App() {
-console.log(firebaseInstance)
-console.log(firebaseInstance.auth())
+  //console.log(firebaseInstance)
+  //console.log(firebaseInstance.auth())
 
+  const [init, setInit] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  
+  useEffect(()=>{
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLogin(true)
+      }else{
+        setIsLogin(false)
+      }
+      setInit(true)
+    });
+  }, [])
   const auth = firebaseInstance.auth();
-  const [isLogin, setIsLogin] = useState(firebaseInstance.currentUser);
   //const [isLogin, setIsLogin] = useState(false);
 
   return (
   <>
-  <AppRouter isLogin={isLogin}/>
+  { init ? <AppRouter isLogin={isLogin}/> : "로딩중~" }
   <footer>&copy; Euitter {new Date().getFullYear()}</footer>
   </>
   )
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,29 +1,49 @@
-import React, { useState } from 'react';
+import { authService } from 'fbase';
+import React, { useEffect, useState } from 'react';
 
 const Auth = () =>{
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [newAccount, setNewAccount] = useState("true");
+const [error, setError] = useState("");
 
 const onChage = (e) =>{
     const {target:{name, value}} = e;
-    console.log(e.target.name)
     if(name === "email"){
-
+        setEmail(value);
     }else if(name === "password"){
-
+        setPassword(value);
     }
 }
-const onSubmit = (e) =>{
+const onSubmit = async (e) =>{
     e.preventDefault(); 
+    try{
+        let data;
+        if(newAccount){
+            //이메일 회원가입
+            data = await authService.createUserWithEmailAndPassword(
+                email, password
+                )
+        }else{
+            //로그인
+            data = await authService.signInWithEmailAndPassword(email, password)
+        }
+    console.log(data)
+    } catch(error){
+        setError(error.message)
+    }
 }
 
+const chkAccount = () => setNewAccount(preV => !preV)
 return (
     <div>
         <h1>Auth</h1>
         <form onSubmit={onSubmit}>
-            <input type="email" name="email" value={email} placeholder="아이디" required value={email} onChange={onChage} />
-            <input type="password" name="password" value={password} placeholder="비밀번호" required value={password} onChange={onChage} />
-            <input type="submit" placeholder="로그인" />
+            <input type="email" name="email" placeholder="아이디" required onChange={onChage} />
+            <input type="password" name="password" placeholder="비밀번호" required onChange={onChage} />
+            <input type="submit" value={newAccount ? "이메일 회원가입" : "로그인"} />
+            <span onClick={chkAccount}>{newAccount ? "로그인" : "회원가입"}</span>
+            <span>{error}</span>
         </form>
         <div>
             <button>GOOGLE 로그인</button>
